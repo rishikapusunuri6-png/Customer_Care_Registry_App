86e34604a9ae33eb29abbc35e99f286139f2996b
rishikapusunuri6-png <rishikapusunuri6@gmail.com>
2026-07-26 12:36:42 +0530

    Add files via upload

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const { registerUser, loginUser, getProfile } = require("../controllers/authController");
+const { protect } = require("../middleware/authMiddleware");
+const { registerValidation, loginValidation } = require("../utils/validators");
+
+router.post("/register", registerValidation, registerUser);
+router.post("/login", loginValidation, loginUser);
+router.get("/me", protect, getProfile);
+
+module.exports = router;
